9801c8e90a8bdf50216167da89ca11382632627d
bhenriq-souza <bhenriq.souza@gmail.com>
2019-02-04 13:46:57 -0200

    Implementing intial dummy structure

--- app.js
+++ app.js
@@ -6,16 +6,33 @@ const { buildSchema } = require('graphql');
 const PORT = 3000;
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
 
 app.use('/graphql', graphqlHttp({
   schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+    }
+
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
 
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -24,8 +41,18 @@ app.use('/graphql', graphqlHttp({
     }
   `),
   rootValue: {
-    events: () => ['Romantic Cooking', 'Sailing', 'All-night coding'],
-    createEvent: args => args.name
+    events: () => events,
+    createEvent: args => {
+      const event = {
+        _id: Math.random().toString(),
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: args.eventInput.date
+      };
+      events.push(event);
+      return event;
+    }
   },
   graphiql: true
 }));
