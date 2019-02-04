36b036411ceb87e54ed1787cd967ea5e26f03956
bhenriq-souza <bhenriq.souza@gmail.com>
2019-02-04 13:17:33 -0200

    initial implemetation GraphQL

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const graphqlHttp = require('express-graphql');
+const { buildSchema } = require('graphql');
 
 const PORT = 3000;
 const app = express();
 
 app.use(bodyParser.json());
 
-/** Default route */
-app.get('/', (req, res, next) => res.send('Hello, world!'));
+app.use('/graphql', graphqlHttp({
+  schema: buildSchema(`
+    type RootQuery {
+      events: [String!]!
+    }
+
+    type RootMutation {
+      createEvent(name: String): String
+    }
+
+    schema {
+      query: RootQuery
+      mutation: RootMutation
+    }
+  `),
+  rootValue: {
+    events: () => ['Romantic Cooking', 'Sailing', 'All-night coding'],
+    createEvent: args => args.name
+  },
+  graphiql: true
+}));
 
 app.listen(PORT);
 console.log(`Server listening to port ${PORT} ...`);
